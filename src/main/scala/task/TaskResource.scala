95d7a95214cfd13ff30437950645e690bd6dcbcb
Peerapat Naksumphan <peerapat.n@arise.tech>
2022-10-13 11:32:45 +0700

    create api to add todo and get todo list

--- src/main/scala/task/TaskResource.scala
+++ src/main/scala/task/TaskResource.scala
@@ -2,7 +2,22 @@ package task
 
 import com.twitter.finatra.http.Controller
 import com.twitter.finagle.http.Request
+import scala.collection.mutable.SortedMap
 
 class TaskResource extends Controller {
-  get("/ping") { req: Request => "pong" }
+  val todoList: SortedMap[Int, Todo] = SortedMap.empty[Int, Todo]
+
+  get("/ping") { _: Request => "pong" }
+
+  post("/todo") { todo: Todo =>
+    val item = todoList.lastOption
+    val id = item match {
+      case Some(value) => value._1 + 1
+      case None        => 0
+    }
+
+    todoList.addOne(id -> Todo(id, todo.detail))
+  }
+
+  get("/todo") { _: Request => todoList.values.toList }
 }
